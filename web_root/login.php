976dd79569f7e44e7c09cf777dc56d4792af3cea
Heath Boutilier <heathboutilier@outlook.com>
2025-08-25 16:58:09 -0700

    login process defined, working on crowlib

--- web_root/login.php
+++ web_root/login.php
@@ -1,7 +1,16 @@
 <?php
+#session start
     if (isset($_POST['submit'])){
         echo $_POST['un'] . " " . $_POST['pw'];
+        #check fail count in session (gt 5, auto fail)
+        #clean inputs
+        #initialize connection to database
+        #if fail > simple error page
+        #test inputs against database
+        #if successful > set username in session
+        #else(fail) > increment fail count in session and display error
     }
+#if username already set in session, redirect
 ?>
 
 <html>
