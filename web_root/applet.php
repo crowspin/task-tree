4a867d4be82a69b7fd034134f6f91fd7b76f1a0c
Heath Boutilier <heathboutilier@outlook.com>
2025-09-02 17:11:24 -0700

    quick notes between dayshift and family visit

---
+++ web_root/applet.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Thinking about changing the crowlib-php SQL folder to IO, renaming SQL classes. Factory, Connection, Data too nondescript.
+ * Learned about Symmetric Array Destructuring today on lunch because PHP doesn't support Tuples.
+ * Also thinking a bit about just having all the classes in the root of crowlib and saving the folders for functions (preferably pure, I don't want includes in there.)
+ * 
+ */
